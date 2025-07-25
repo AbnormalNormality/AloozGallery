ce19d3254875cfa4e5cd92faaf24f892e06b1cc4
AbnormalNormality <dan.driscoll@aussiebb.com.au>
2025-07-25 22:07:31 +1000

    Added dex entries, species, and abilities Added Nidoqueen and Nidoking lines

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,9 +11,14 @@ function displayGallery() {
     for (const p of sortedPokemon) {
         const slide = document.createElement("div");
         slide.classList.add("slide");
+        const nameWrapper = document.createElement("div");
+        nameWrapper.classList.add("name-wrapper");
         const name = document.createElement("div");
         name.classList.add("name");
         name.textContent = p.name;
+        const title = document.createElement("div");
+        title.classList.add("title");
+        title.textContent = p.title;
         const typeWrapper = document.createElement("div");
         typeWrapper.classList.add("type-wrapper");
         const typeWrapperInner = document.createElement("div");
@@ -28,12 +33,31 @@ function displayGallery() {
             typeWrapperInner.style.setProperty(`--type${i}-foreground`, data.foreground);
             typeWrapperInner.append(type);
         }
+        const pictureWrapper = document.createElement("div");
+        pictureWrapper.classList.add("picture-wrapper");
         const picture = document.createElement("img");
         picture.classList.add("picture");
         picture.loading = "lazy";
         picture.src = p.picture;
+        const entries = document.createElement("div");
+        entries.classList.add("entries");
+        for (const e of p.entries) {
+            const entry = document.createElement("div");
+            entry.textContent = e;
+            entries.append(entry);
+        }
+        const abilities = document.createElement("div");
+        abilities.classList.add("abilities");
+        for (const a of p.abilities) {
+            const data = getData("abilities")[a];
+            const ability = document.createElement("div");
+            ability.textContent = data.name;
+            abilities.append(ability);
+        }
+        nameWrapper.append(name, title);
         typeWrapper.append(typeWrapperInner);
-        slide.append(name, typeWrapper, picture);
+        pictureWrapper.append(picture);
+        slide.append(nameWrapper, typeWrapper, pictureWrapper, abilities, entries);
         slides.push(slide);
     }
     galleryDiv.append(...slides);
